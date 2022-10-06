f6768772333c7f13e224c185476f973660f4faea
selina <torsoxs@gmail.com>
2022-10-06 13:30:16 -0500

    finished first part of basics

---
+++ class-11-typescript/basics.ts
@@ -0,0 +1,17 @@
+// Primitave: number, string, boolean
+// More complex types: arrays, objrects
+// Function types, paremeters
+
+// Primatives
+
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = 'Max';
+
+let isInstructor: boolean;
+
+isInstructor = true;
